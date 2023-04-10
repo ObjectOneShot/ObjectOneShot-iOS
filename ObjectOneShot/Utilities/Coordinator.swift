e9f3065a8a2169886a3ddd7ea812295e8170029d
KSYong <ericyong95@gmail.com>
2023-04-11 08:54:05 +0900

    [feat] : NavigationStack 사용 위한 NavigationPath Coordinator 구현

---
+++ ObjectOneShot/Utilities/Coordinator.swift
@@ -0,0 +1,20 @@
+//
+//  Coordinator.swift
+//  ObjectOneShot
+//
+//  Created by 권승용 on 2023/04/08.
+//
+
+import SwiftUI
+
+class Coordinator: ObservableObject {
+    @Published var path = NavigationPath()
+
+    func show<V>(_ viewType: V.Type) where V: View {
+        path.append(String(describing: viewType.self))
+    }
+
+    func popToRoot() {
+        path.removeLast(path.count)
+    }
+}
