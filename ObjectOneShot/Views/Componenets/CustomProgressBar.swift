51861e406198cad58e01f68eb7339a795380d947
Kwon Seung Yong <ericyong95@gmail.com>
2023-04-15 18:30:33 +0900

    [feat] 커스텀 프로그레스 바 구현

---
+++ ObjectOneShot/Views/Componenets/CustomProgressBar.swift
@@ -0,0 +1,45 @@
+//
+//  CustomProgressView.swift
+//  ObjectOneShot
+//
+//  Created by 권승용 on 2023/04/14.
+//
+
+import SwiftUI
+
+
+struct CustomProgressBar: View {
+    @Binding var value: Double
+    var backgroundColor: Color
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle().frame(width: geometry.size.width, height: geometry.size.height)
+                    .foregroundColor(backgroundColor)
+                
+                Rectangle().frame(width: min(CGFloat(self.value) * geometry.size.width, geometry.size.width), height: geometry.size.height)
+                        .if(value <= 0.33) { view in
+                            view
+                                .foregroundColor(Color("point_1"))
+                        }
+                        .if(value > 0.33 && value <= 0.66) { view in
+                            view
+                                .foregroundColor(Color("point_2"))
+                        }
+                        .if(value > 0.66) { view in
+                            view
+                                .foregroundColor(Color("point_3"))
+                        }
+                        .animation(.linear, value: value)
+            }
+            .cornerRadius(45.0)
+        }
+    }
+}
+
+struct CustomProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomProgressBar(value: .constant(0.3), backgroundColor: Color("grey_200"))
+    }
+}
